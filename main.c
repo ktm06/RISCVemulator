66d1b7d293f3206b423300bc061f485eed3c012f
ktm06 <mikikippei@gmail.com>
2026-06-21 13:09:57 -0400

    test tweak

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void) {
     static struct CPU cpu;
     reset(&cpu);
 
-    loadfile(&cpu, "tests/bins/divzero.bin");
+    loadfile(&cpu, "tests/bins/test.bin");
     run(&cpu);
     regview(&cpu);
     return 0;
