854d837a738f42293739fe4b379d4a19ff9976c3
ktm06 <mikikippei@gmail.com>
2026-06-29 21:37:32 -0400

    set up instr table, wrote encode R function, set up skeleton for run2

--- assembler/assembler.ml
+++ assembler/assembler.ml
@@ -1,13 +1,70 @@
 open Str
 
-let rtype = ["add";"sub";"sll";"slt";"sltu";"xor";"srl";"or";"and"]
-let mtype = ["mul";"mulh";"mulhsu";"mulhu";"div";"divu";"rem";"remu"]
-let itype = ["addi";"slli";"slti";"sltiu";"xori";"srli";"srai";"ori"]
-let ltype = ["lb";"lh";"lw";"lbu";"lhu"]
-let stype = ["sb";"sh";"sw"]
-let btype = ["beq";"bne";"blt";"bge";"bltu";"bgeu";]
-let jalr = ["jalr"]
-let jal = ["jal"]
+type instrtype = Rtype|Mtype|Itype|Ltype|Stype|Btype|Jal|Jalr
+
+(* ( opcode,funct3,funct7 )*)
+let table = [ 
+  ("add",    (0x33, 0x0, 0x00));
+  ("sub",    (0x33, 0x0, 0x20));
+  ("sll",    (0x33, 0x1, 0x00));
+  ("slt",    (0x33, 0x2, 0x00));
+  ("sltu",   (0x33, 0x3, 0x00));
+  ("xor",    (0x33, 0x4, 0x00));
+  ("srl",    (0x33, 0x5, 0x00));
+  ("sra",    (0x33, 0x5, 0x20));
+  ("or",     (0x33, 0x6, 0x00));
+  ("and",    (0x33, 0x7, 0x00));
+  ("mul",    (0x33, 0x0, 0x01));
+  ("mulh",   (0x33, 0x1, 0x01));
+  ("mulhsu", (0x33, 0x2, 0x01));
+  ("mulhu",  (0x33, 0x3, 0x01));
+  ("div",    (0x33, 0x4, 0x01));
+  ("divu",   (0x33, 0x5, 0x01));
+  ("rem",    (0x33, 0x6, 0x01));
+  ("remu",   (0x33, 0x7, 0x01));
+  ("addi",   (0x13, 0x0, 0x00));
+  ("slti",   (0x13, 0x2, 0x00));
+  ("sltiu",  (0x13, 0x3, 0x00));
+  ("xori",   (0x13, 0x4, 0x00));
+  ("ori",    (0x13, 0x6, 0x00));
+  ("andi",   (0x13, 0x7, 0x00));
+  ("slli",   (0x13, 0x1, 0x00));
+  ("srli",   (0x13, 0x5, 0x00));
+  ("srai",   (0x13, 0x5, 0x20));
+  ("lb",     (0x03, 0x0, 0x00));
+  ("lh",     (0x03, 0x1, 0x00));
+  ("lw",     (0x03, 0x2, 0x00));
+  ("lbu",    (0x03, 0x4, 0x00));
+  ("lhu",    (0x03, 0x5, 0x00));
+  ("sb",     (0x23, 0x0, 0x00));
+  ("sh",     (0x23, 0x1, 0x00));
+  ("sw",     (0x23, 0x2, 0x00));
+  ("beq",    (0x63, 0x0, 0x00));
+  ("bne",    (0x63, 0x1, 0x00));
+  ("blt",    (0x63, 0x4, 0x00));
+  ("bge",    (0x63, 0x5, 0x00));
+  ("bltu",   (0x63, 0x6, 0x00));
+  ("bgeu",   (0x63, 0x7, 0x00));
+  ("jal",    (0x6F, 0x0, 0x00));  
+  ("jalr",   (0x67, 0x0, 0x00));  
+]
+
+let check instr = 
+  let (opcode, _, funct7) = List.assoc instr table in match opcode with
+  | 0x33 -> if funct7=0x01 then Mtype else Rtype
+  | 0x13 -> Itype
+  | 0x03 -> Ltype
+  | 0x23 -> Stype
+  | 0x63 -> Btype
+  | 0x6F -> Jal
+  | 0x67 -> Jalr
+  | _ -> failwith ("unknown opcode: " ^ instr)
+
+let encodeR instr rd rs1 rs2 = 
+  let (opcode, funct3, funct7) = List.assoc instr table in opcode
+  lor (rd lsl 7) lor (funct3 lsl 12) lor (rs1 lsl 15) lor (rs2 lsl 20) lor (funct7 lsl 25)
+
+
 
 
 let contents = In_channel.with_open_text "test1.txt" In_channel.input_all
@@ -33,7 +90,7 @@ let symbols = Hashtbl.create 10
 
 let insertsymbol input = Hashtbl.replace symbols input !increment
 
-let run1 input = List.iter ( fun x-> 
+let run1 input = List.iter ( fun x -> 
   match x with
   | [] -> ()
   | h :: t -> 
@@ -43,6 +100,22 @@ let run1 input = List.iter ( fun x->
     end else acc()
 ) input
 
+(*
+let run2 input = List.iter ( fun x ->
+  match x with 
+  | [] -> ()
+  | h :: t -> 
+    begin 
+    if (check h)=Rtype
+      encodeR
+      
+      
+    
+  )
+*) 
+
+
+(* pipeline here *)
 let parsed = contents |> filesplitter |> commstrip |> linesplitter |> stripempty 
 
 let () = run1 parsed
